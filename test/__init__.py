cd75739ee6d85b5803c90a2964b34e30db6538f2
Adam Carpenter <adamc@jlab.org>
2022-10-06 12:46:30 -0400

    Don't need to leverage a config file here

--- test/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from rfwtools.config import Config
-
-
-# Get rfwtools to read the config in the test directory
-file = os.path.join(os.path.dirname(__file__), 'rfwtools.cfg')
-Config()
-Config().read_config_file(file)
